37304d432af457cf2bf7c1b4b02039cf25749684
Michael McCandless <lucene@mikemccandless.com>
2010-11-09 05:25:50 -0500

    rework repeatLuceneTest

--- repeatLuceneTest.py
+++ repeatLuceneTest.py
@@ -3,96 +3,173 @@ import datetime
 import os
 import sys
 
+# NOTE
+#   - only works in the lucene subdir, ie this runs equivalent of "ant test-core"
+
+IS_WINDOWS = sys.platform.find('win') != -1 and sys.platform.find('darwin') == -1
+
+JAVA_ARGS = '-Xmx512m -Xms512m'
+
 if 0:
-  tests = ('org.apache.lucene.index.TestThreadedOptimize',
-           'org.apache.lucene.index.TestConcurrentMergeScheduler',
-           'org.apache.lucene.index.TestTransactions',
-           'org.apache.lucene.index.TestAddIndexesNoOptimize',
-           'org.apache.lucene.index.stresstests.TestIndexWriterConcurrent',
-           'org.apache.lucene.index.TestStressIndexing',
-           'org.apache.lucene.index.TestStressIndexing2',
-           'org.apache.lucene.index.TestIndexWriter',
-           'org.apache.lucene.index.TestAtomicUpdate',
-           'org.apache.lucene.index.TestIndexWriterDelete',
-           'org.apache.lucene.index.TestIndexWriterExceptions',
-           'org.apache.lucene.TestSnapshotDeletionPolicy',
-           'org.apache.lucene.index.TestCrash')
+  if 0:
+    tests = ('org.apache.lucene.index.TestThreadedOptimize',
+             'org.apache.lucene.index.TestConcurrentMergeScheduler',
+             'org.apache.lucene.index.TestTransactions',
+             'org.apache.lucene.index.TestAddIndexesNoOptimize',
+             'org.apache.lucene.index.stresstests.TestIndexWriterConcurrent',
+             'org.apache.lucene.index.TestStressIndexing',
+             'org.apache.lucene.index.TestStressIndexing2',
+             'org.apache.lucene.index.TestIndexWriter',
+             'org.apache.lucene.index.TestAtomicUpdate',
+             'org.apache.lucene.index.TestIndexWriterDelete',
+             'org.apache.lucene.index.TestIndexWriterExceptions',
+             'org.apache.lucene.TestSnapshotDeletionPolicy',
+             'org.apache.lucene.index.TestCrash')
 
-  tests = ('org.apache.lucene.search.TestRegexpRandom2',
-           'org.apache.lucene.search.TestRegexpRandom',
-           'org.apache.lucene.search.TestPrefixRandom',
-           'org.apache.lucene.search.TestAutomatonQuery',
-           'org.apache.lucene.util.automaton.TestUTF32ToUTF8',
-           'org.apache.lucene.index.codecs.preflex.TestSurrogates')
-  doLog = False
-else:
-  #tests = ('org.apache.lucene.index.TestAtomicUpdate',)
-  #tests = ('org.apache.lucene.index.TestBackwardsCompatibility',)
-  #tests = ('org.apache.lucene.search.TestPrefixRandom',)
-  #tests = ('org.apache.lucene.index.codecs.preflex.TestSurrogates',)
-  #logDirName = 'atomic'
-  #tests = ('org.apache.lucene.index.TestPayloads',)
-  tests = ('org.apache.lucene.index.TestIndexWriterExceptions',)
-  #tests = ('org.apache.lucene.index.TestConcurrentMergeScheduler',)
-  logDirName = '/tmp/logs/%s' % os.path.split(os.getcwd())[-1]
-  doLog = True
+    tests = ('org.apache.lucene.search.TestRegexpRandom2',
+             'org.apache.lucene.search.TestRegexpRandom',
+             'org.apache.lucene.search.TestPrefixRandom',
+             'org.apache.lucene.search.TestAutomatonQuery',
+             'org.apache.lucene.util.automaton.TestUTF32ToUTF8',
+             'org.apache.lucene.index.codecs.preflex.TestSurrogates')
+    doLog = False
+  else:
+    #tests = ('org.apache.lucene.index.TestAtomicUpdate',)
+    #tests = ('org.apache.lucene.index.TestBackwardsCompatibility',)
+    #tests = ('org.apache.lucene.search.TestPrefixRandom',)
+    #tests = ('org.apache.lucene.index.codecs.preflex.TestSurrogates',)
+    #logDirName = 'atomic'
+    #tests = ('org.apache.lucene.index.TestPayloads',)
+    tests = ('org.apache.lucene.index.TestIndexWriterExceptions',)
+    #tests = ('org.apache.lucene.index.TestConcurrentMergeScheduler',)
+    doLog = True
 
-if os.system('ant compile-core compile-test common.compile-test'):
-  sys.exit(0)
-  
+allTests = {}
+def locateTest(test):
+  cwd = os.getcwd()
+  tup = test.split('.')
+  if len(tup) == 1:
+    method = None
+  else:
+    test, method = tup
+  if len(allTests) == 0:
+    for root, dirs, files in os.walk('src/test'):
+      for f in files:
+        if f.endswith('.java') and (f.startswith('Test') or f.endswith('Test.java')):
+          path = root[len('src/test/'):].replace(os.sep, '.')
+          allTests[f[:-5]] = '%s.%s' % (path, f[:-5])
+          #print '%s.%s' % (path, f[:-5])
+  if test not in allTests:
+    return None
+  else:
+    return allTests[test], method
+
+def getArg(argName, default, hasArg=True):
+  try:
+    idx = sys.argv.index(argName)
+  except ValueError:
+    v = default
+  else:
+    if hasArg:
+      v = sys.argv[idx+1]
+      del sys.argv[idx:idx+2]
+    else:
+      v = True
+      del sys.argv[idx]
+  return v
+    
+logDirName = '/tmp/logs/%s' % os.path.split(os.getcwd())[-1]
+if IS_WINDOWS:
+  logDirName = 'c:' + logDirName
+doLog = True
+
+print 'Logging to dir %s' % logDirName
+
 if doLog:
   if os.path.exists(logDirName):
     shutil.rmtree(logDirName)
   os.makedirs(logDirName)
 
-IS29 = False
+print 'Compile...'
+if os.system('ant compile-core compile-test common.compile-test > compile.log 2>&1'):
+  print open('compile.log', 'rb').read()
+  sys.exit(1)
+  
+OLD_JUNIT = os.path.exists('lib/junit-3.8.2.jar')
 
-p = sys.platform.lower()
-if not IS29:
+mult = int(getArg('-mult', 1))
+codec = getArg('-codec', 'random')
+dir = getArg('-dir', 'random')
+verbose = getArg('-verbose', False, False)
+iters = int(getArg('-iters', 1))
+
+if len(sys.argv) == 1:
+  print 'ERROR: no test specified'
+  sys.exit(1)
+
+tests = []
+for test in sys.argv[1:]:
+  if not test.startswith('org.'):
+    tup = locateTest(test)
+    if tup is None:
+      print 'ERROR: cannot find test %s' % test
+      sys.exit(1)
+    testClass, testMethod = tup
+    tests.append((testClass, testMethod))
+  
+CP = []
+CP.append('build/classes/test')
+CP.append('build/classes/java')
+if not OLD_JUNIT:
   JUNIT_JAR = './lib/junit-4.7.jar'
-  #JUNIT_JAR = '/Volumes/root/usr/local/src/junit-4.4.jar'
-  CP = '-cp .:%s:./build/classes/test:./build/classes/java' % JUNIT_JAR
 else:
-  CP = '-cp .:./lib/junit-3.8.2.jar:./build/classes/test:./build/classes/java:./build/classes/demo'
-#ARGS = '-server -Xmx1024m -Xms1024m -Xbatch %s' % CP
-ARGS = '-Xmx512m -Xms512m %s' % CP
-#ARGS = CP
+  JUNIT_JAR = './lib/junit-3.8.2.jar'
+CP.append(JUNIT_JAR)
+if os.path.exists('build/classes/demo'):
+  CP.append('build/classes/demo')
 
-ranMult = 1
-verbose = 'false'
-codec = 'random'
-#codec = 'PreFlex'
-#codec = '"MockVariableIntBlock(29)"'
+JAVA_ARGS += ' -cp "%s"' % os.pathsep.join(CP)
+
+
+
 upto = 0
 while True:
-  for test in tests:
+  for testClass, testMethod in tests:
     print
-    if doLog:
-      print '%s TEST: %s -> %s/%d.log' % (datetime.datetime.now(), test, logDirName, upto)
+    if testMethod is not None:
+      s = '%s#%s' % (testClass, testMethod)
     else:
-      print '%s TEST: %s' % (datetime.datetime.now(), test)
+      s = testClass
+
+    if doLog:
+      print '%s TEST: %s -> %s/%d.log' % (datetime.datetime.now(), s, logDirName, upto)
+    else:
+      print '%s TEST: %s' % (datetime.datetime.now(), s)
       
-    if doLog:
-      if IS29:
-        command = 'java %s -DtempDir=build/test -ea junit.textui.TestRunner %s > %s/%d.log 2>&1' % (ARGS, test, logDirName, upto)
-      else:
-        #command = 'java %s -DtempDir=build/test -Dtests.codec=%s -Dtestmethod=testRandomStoredFields -Dtests.verbose=%s -Drandom.multiplier=%d -Dtests.iter=50 -ea org.junit.runner.JUnitCore %s > %s/%d.log 2>&1' % (ARGS, codec, verbose, ranMult, test, logDirName, upto)
-        #command = 'java %s -DtempDir=build/test -Dtestmethod=testRandomStoredFields -Dtests.verbose=%s -Drandom.multiplier=%d -Dtests.iter=10 -ea org.junit.runner.JUnitCore %s > %s/%d.log 2>&1' % (ARGS, verbose, ranMult, test, logDirName, upto)
-        #command = 'java %s -DtempDir=build/test -Dtests.directory=SimpleFSDirectory -Dtests.verbose=%s -Drandom.multiplier=%d -Dtests.iter=100 -ea org.junit.runner.JUnitCore %s > %s/%d.log 2>&1' % (ARGS, verbose, ranMult, test, logDirName, upto)
-        #command = 'java %s -DtempDir=build/test -Drandom.multiplier=%d -ea org.junit.runner.JUnitCore %s > %s/%d.log 2>&1' % (ARGS, ranMult, test, logDirName, upto)
-        command = 'java %s -DtempDir=build/test -Dtests.verbose=%s -Drandom.multiplier=%d -Dtests.iter=10 -Dtests.codec=%s -Dtests.directory=RAMDirectory -ea org.junit.runner.JUnitCore %s > %s/%d.log 2>&1' % (ARGS, verbose, ranMult, codec, test, logDirName, upto)
-      print 'CLEAN'
-      if os.path.exists('build/test'):
-        shutil.rmtree('build/test')
-      print 'RUN: %s' % command
-      res = os.system(command)
-      #res = os.system('ant test-core -Dtestcase=%s' % test.split('.')[-1])
+    command = 'java %s -DtempDir=build/test -ea' % JAVA_ARGS
+    command += ' -Dtests.verbose=%s' % verbose
+    command += ' -Drandom.multiplier=%s' % mult
+    command += ' -Dtests.iter=%s' % iters
+    command += ' -Dtests.codec=%s' % codec
+    command += ' -Dtests.directory=%s' % dir
+    if testMethod is not None:
+      command += ' -Dtestmethod=%s' % testMethod
+      
+    if OLD_JUNIT:
+      command += ' junit.textui.TestRunner'
     else:
-      #res = os.system('java -server -agentlib:yjpagent -Xmx1024m -Xms1024m -Xbatch -DtempDir=build/test -ea org.junit.runner.JUnitCore %s' % test)
-      command = 'java %s -DtempDir=build/test -Drandom.multiplier=%s -ea org.junit.runner.JUnitCore %s' % (ARGS, ranMult, test)
-      #command = 'ant test-core -Dtestcase=%s' 
-      print 'RUN: %s' % command
-      res = os.system(command)
+      command += ' org.junit.runner.JUnitCore'
+
+    command += ' %s' % testClass
+
+    if doLog:
+      command += ' > %s/%d.log 2>&1' % (logDirName, upto)
+      
+    print '  remove build/test'
+    if os.path.exists('build/test'):
+      shutil.rmtree('build/test')
+    print '  RUN: %s' % command
+    res = os.system(command)
 
     if res:
       print '  FAILED'

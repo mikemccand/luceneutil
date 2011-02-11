a206ef1698aca7e1c52369ccdc7fbd1506dea5f6
Michael McCandless <lucene@mikemccandless.com>
2011-02-11 12:37:36 -0500

    initial Indexer class, to replace using modules/benchmark (not hooked into benchUtil.py yet)

---
+++ perf/Indexer.java
@@ -0,0 +1,231 @@
+package perf;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.*;
+import org.apache.lucene.index.IndexReader.AtomicReaderContext;
+import org.apache.lucene.search.*;
+import org.apache.lucene.analysis.*;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.standard.ClassicAnalyzer;
+import org.apache.lucene.store.*;
+import org.apache.lucene.document.*;
+import org.apache.lucene.util.*;
+
+// javac -Xlint:deprecation -cp ../modules/analysis/build/common/classes/java:build/classes/java:build/classes/test perf/Indexer.java perf/LineFileDocs.java
+
+// Usage: dirImpl dirPath analyzer /path/to/line/file numDocs numThreads doOptimize:yes|no verbose:yes|no ramBufferMB maxBufferedDocs
+
+// EG:
+//
+//  java -cp .:../modules/analysis/build/common/classes/java:build/classes/java:build/classes/test perf.Indexer NIOFSDirectory /lucene/indices/test StandardAnalyzer /p/lucene/data/enwiki-20110115-lines-1k-fixed.txt 1000000 6 no yes 256.0 -1
+
+public final class Indexer {
+  public static void main(String[] args) throws Exception {
+
+    final String dirImpl = args[0];
+    final String dirPath = args[1];
+
+    final Directory dir;
+    if (dirImpl.equals("MMapDirectory")) {
+      dir = new MMapDirectory(new File(dirPath));
+    } else if (dirImpl.equals("NIOFSDirectory")) {
+      dir = new NIOFSDirectory(new File(dirPath));
+    } else if (dirImpl.equals("SimpleFSDirectory")) {
+      dir = new SimpleFSDirectory(new File(dirPath));
+    } else {
+      throw new RuntimeException("unknown directory impl \"" + dirImpl + "\"");
+    }
+      
+    final String analyzer = args[2];
+    final Analyzer a;
+    if (analyzer.equals("EnglishAnalyzer")) {
+      a = new EnglishAnalyzer(Version.LUCENE_31);
+    } else if (analyzer.equals("ClassicAnalyzer")) {
+      a = new ClassicAnalyzer(Version.LUCENE_30);
+    } else if (analyzer.equals("StandardAnalyzer")) {
+      a = new StandardAnalyzer(Version.LUCENE_40);
+    } else {
+      throw new RuntimeException("unknown analyzer " + analyzer);
+    } 
+
+    final String lineFile = args[3];
+
+    // -1 means all docs in the line file:
+    final int docCount = Integer.parseInt(args[4]);
+    final int numThreads = Integer.parseInt(args[5]);
+
+    final boolean doOptimize = args[6].equals("yes");
+    final boolean verbose = args[7].equals("yes");
+
+    final double ramBufferSizeMB = Double.parseDouble(args[8]);
+    final int maxBufferedDocs = Integer.parseInt(args[9]);
+
+    System.out.println("Dir: " + dirImpl);
+    System.out.println("Index path: " + dirPath);
+    System.out.println("Analyzer: " + analyzer);
+    System.out.println("Line file: " + lineFile);
+    System.out.println("Doc count: " + (docCount == -1 ? "all docs" : ""+docCount));
+    System.out.println("Threads: " + numThreads);
+    System.out.println("Optimize: " + (doOptimize ? "yes" : "no"));
+    System.out.println("Verbose: " + (verbose ? "yes" : "no"));
+    System.out.println("RAM Buffer MB: " + ramBufferSizeMB);
+    System.out.println("Max buffered docs: " + maxBufferedDocs);
+
+    final IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_40, a);
+    iwc.setMaxBufferedDocs(maxBufferedDocs);
+    iwc.setRAMBufferSizeMB(ramBufferSizeMB);
+
+    // We want deterministic merging, since we target a multi-seg index w/ 5 segs per level:
+    iwc.setMergePolicy(new LogDocMergePolicy());
+    ((LogMergePolicy) iwc.getMergePolicy()).setUseCompoundFile(false);
+
+    // Keep all commit points:
+    iwc.setIndexDeletionPolicy(NoDeletionPolicy.INSTANCE);
+
+    final IndexWriter w = new IndexWriter(dir, iwc);
+    w.setInfoStream(verbose ? System.out : null);
+
+    final LineFileDocs docs = new LineFileDocs(lineFile);
+
+    final long t0 = System.currentTimeMillis();
+    final Thread[] threads = new Thread[numThreads];
+    for(int thread=0;thread<numThreads;thread++) {
+      threads[thread] = new IndexThread(w, docs, docCount);
+      threads[thread].start();
+    }
+    for(int thread=0;thread<numThreads;thread++) {
+      threads[thread].join();
+    }
+    docs.close();
+
+    final long t1 = System.currentTimeMillis();
+    System.out.println("\nIndexer: indexing done (" + (t1-t0) + " msec); total " + w.maxDoc() + " docs");
+
+    if (docCount != -1 && w.maxDoc() != docCount) {
+      throw new RuntimeException("w.maxDoc()=" + w.maxDoc() + " but expected " + docCount);
+    }
+
+    w.waitForMerges();
+    final long t2 = System.currentTimeMillis();
+    System.out.println("\nIndexer: waitForMerges done (" + (t2-t1) + " msec)");
+
+    final Map<String,String> commitData = new HashMap<String,String>();
+    commitData.put("userData", "multi");
+    w.commit(commitData);
+    final long t3 = System.currentTimeMillis();
+    System.out.println("\nIndexer: commit multi (took " + (t3-t2) + " msec)");
+
+    // Randomly delete 5% of the docs
+    final Set<Integer> deleted = new HashSet<Integer>();
+    final int maxDoc = w.maxDoc();
+    final int toDeleteCount = (int) (maxDoc * 0.05);
+    System.out.println("\nIndexer: delete " + toDeleteCount + " docs");
+    final Random rand = new Random(17);
+    while(deleted.size() < toDeleteCount) {
+      final int id = rand.nextInt(maxDoc);
+      if (!deleted.contains(id)) {
+        deleted.add(id);
+        w.deleteDocuments(new Term("id", Integer.toString(id)));
+      }
+    }
+    final long t4 = System.currentTimeMillis();
+    System.out.println("\nIndexer: deletes done (took " + (t4-t3) + " msec)");
+
+    commitData.put("userData", "delmulti");
+    w.commit(commitData);
+    final long t5 = System.currentTimeMillis();
+    System.out.println("\nIndexer: commit delmulti done (took " + (t5-t4) + " msec)");
+
+    if (w.numDocs() != maxDoc - toDeleteCount) {
+      throw new RuntimeException("count mismatch: w.numDocs()=" + w.numDocs() + " but expected " + (maxDoc - toDeleteCount));
+    }
+
+    if (doOptimize) {
+      w.optimize();
+      final long t6 = System.currentTimeMillis();
+      System.out.println("\nIndexer: optimize done (took " + (t6-t5) + " msec)");
+
+      commitData.put("userData", "single");
+      w.commit();
+      final long t7 = System.currentTimeMillis();
+      System.out.println("\nIndexer: commit single done (took " + (t7-t6) + " msec)");
+
+      final int maxDoc2 = w.maxDoc();
+      if (maxDoc2 != maxDoc - toDeleteCount) {
+        throw new RuntimeException("count mismatch: w.maxDoc()=" + w.maxDoc() + " but expected " + (maxDoc - toDeleteCount));
+      }
+      final int toDeleteCount2 = (int) (maxDoc2 * 0.05);
+      System.out.println("\nIndexer: delete " + toDeleteCount + " docs");
+      while(deleted.size() < toDeleteCount) {
+        final int id = rand.nextInt(maxDoc);
+        if (!deleted.contains(id)) {
+          deleted.add(id);
+          w.deleteDocuments(new Term("id", Integer.toString(id)));
+        }
+      }
+      final long t8 = System.currentTimeMillis();
+      System.out.println("\nIndexer: deletes done (took " + (t8-t7) + " msec)");
+
+      commitData.put("userData", "delsingle");
+      w.commit(commitData);
+      final long t9 = System.currentTimeMillis();
+      System.out.println("\nIndexer: commit delsingle done (took " + (t9-t8) + " msec)");
+    }
+
+    w.close();
+    dir.close();
+  }
+
+  private static class IndexThread extends Thread {
+    private final LineFileDocs docs;
+    private final int numTotalDocs;
+    private final IndexWriter w;
+    private final LineFileDocs.DocState docState;
+
+    public IndexThread(IndexWriter w, LineFileDocs docs, int numTotalDocs) {
+      this.w = w;
+      this.docs = docs;
+      this.docState = docs.newDocState();
+      this.numTotalDocs = numTotalDocs;
+    }
+
+    @Override
+    public void run() {
+      final Field idField = docState.id;
+      while(true) {
+        try {
+          final Document doc = docs.nextDoc(docState);
+          if (doc == null ||
+              (numTotalDocs != -1 && Integer.parseInt(idField.stringValue()) >= numTotalDocs)) {
+            break;
+          }
+          w.addDocument(doc);
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+      }
+    }
+  }
+}
---
+++ perf/LineFileDocs.java
@@ -0,0 +1,138 @@
+package perf;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
+import java.text.SimpleDateFormat;
+import java.text.ParsePosition;
+import org.apache.lucene.document.*;
+
+public class LineFileDocs implements Closeable {
+
+  private final BufferedReader reader;
+  private final static int BUFFER_SIZE = 1 << 16;     // 64K
+
+  public LineFileDocs(String path) throws IOException {
+    final InputStream is = new FileInputStream(path);
+    final InputStream in = new BufferedInputStream(is, BUFFER_SIZE);
+    reader = new BufferedReader(new InputStreamReader(in, "UTF-8"), BUFFER_SIZE);
+  }
+
+  public synchronized void close() throws IOException {
+    reader.close();
+  }
+
+  private final static char SEP = '\t';
+
+  public static final class DocState {
+    final Document doc;
+    final Field titleTokenized;
+    final Field title;
+    final Field body;
+    final Field id;
+    final Field date;
+    final NumericField dateMSec;
+    final NumericField timeSec;
+    final SimpleDateFormat dateParser = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss", Locale.US);
+    final Calendar dateCal = Calendar.getInstance();
+    final ParsePosition datePos = new ParsePosition(0);
+
+    public DocState() {
+      doc = new Document();
+      
+      title = new Field("title", "", Field.Store.NO, Field.Index.NOT_ANALYZED_NO_NORMS);
+      title.setOmitTermFreqAndPositions(true);
+      doc.add(title);
+
+      titleTokenized = new Field("titleTokenized", "", Field.Store.YES, Field.Index.ANALYZED);
+      doc.add(titleTokenized);
+
+      body = new Field("body", "", Field.Store.YES, Field.Index.ANALYZED);
+      doc.add(body);
+
+      id = new Field("id", "", Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS);
+      id.setOmitTermFreqAndPositions(true);
+      doc.add(id);
+
+      date = new Field("date", "", Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS);
+      date.setOmitTermFreqAndPositions(true);
+      doc.add(date);
+
+      dateMSec = new NumericField("datenum");
+      timeSec = new NumericField("timesecnum");
+    }
+  }
+
+  public DocState newDocState() {
+    return new DocState();
+  }
+
+  private final ThreadLocal<DocState> threadDocs = new ThreadLocal<DocState>();
+
+  private int readCount;
+
+  public Document nextDoc(DocState doc) throws IOException {
+    String line;
+    final int myID;
+    synchronized(this) {
+      myID = readCount++;
+      line = reader.readLine();
+      if (line == null) {
+        return null;
+      }
+    }
+
+    int spot = line.indexOf(SEP);
+    if (spot == -1) {
+      throw new RuntimeException("line: [" + line + "] is in an invalid format !");
+    }
+    int spot2 = line.indexOf(SEP, 1 + spot);
+    if (spot2 == -1) {
+      throw new RuntimeException("line: [" + line + "] is in an invalid format !");
+    }
+
+    doc.body.setValue(line.substring(1+spot2, line.length()));
+    final String title = line.substring(0, spot);
+    doc.title.setValue(title);
+    doc.titleTokenized.setValue(title);
+    final String dateString = line.substring(1+spot, spot2);
+    doc.date.setValue(dateString);
+    doc.id.setValue(Integer.toString(myID));
+
+    doc.datePos.setIndex(0);
+    final Date date = doc.dateParser.parse(dateString, doc.datePos);
+    doc.dateMSec.setLongValue(date.getTime());
+
+    doc.dateCal.setTime(date);
+    final int sec = doc.dateCal.get(Calendar.HOUR_OF_DAY)*3600 + doc.dateCal.get(Calendar.MINUTE)*60 + doc.dateCal.get(Calendar.SECOND);
+    doc.timeSec.setIntValue(sec);
+
+    return doc.doc;
+  }
+}
+

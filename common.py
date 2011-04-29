7ccf9bd7f8f7f022cf2bb04490b796b869110579
Michael McCandless <lucene@mikemccandless.com>
2011-04-29 13:58:40 -0400

    refactoring

--- common.py
+++ common.py
@@ -17,3 +17,48 @@ def pathsep():
     return ';'
   else:
     return os.pathsep
+
+allTests = {}
+
+def locateTest(test):
+  if not os.path.exists('src/test'):
+    raise RuntimeError('cwd must be $ROOT/lucene of src checkout')
+
+  cwd = os.getcwd()
+  tup = test.split('.')
+  if len(tup) == 1:
+    method = None
+  else:
+    test, method = tup
+  if len(allTests) == 0:
+    for root, dirs, files in os.walk('src/test'):
+      for f in files:
+        if f.endswith('.java') and (f.startswith('Test') or f.endswith('Test.java')):
+          path = root[len('src/test/'):].replace(os.sep, '.')
+          allTests[f[:-5]] = '%s.%s' % (path, f[:-5])
+          #print '%s.%s' % (path, f[:-5])
+  if test not in allTests:
+    return None
+  else:
+    return allTests[test], method
+
+def getTestClassPath(ROOT):
+  CP = []
+  CP.append(ROOT+'build/classes/test')
+  CP.append(ROOT+'build/classes/test-framework')
+  CP.append(ROOT+'build/classes/java')
+
+  if not os.path.exists('lib/junit-3.8.2.jar'):
+    JUNIT_JAR = 'lib/junit-4.7.jar'
+  else:
+    JUNIT_JAR = 'lib/junit-3.8.2.jar'
+  CP.append(ROOT + JUNIT_JAR)
+  if os.path.exists(ROOT + 'build/classes/demo'):
+    CP.append(ROOT + 'build/classes/demo')
+
+  if ROOT != '':
+    CP.append(ROOT + 'build/contrib/queries/classes/java')
+    CP.append(ROOT + 'build/contrib/queries/classes/test')
+
+  return CP
+

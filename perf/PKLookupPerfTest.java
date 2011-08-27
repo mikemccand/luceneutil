da81ab92bface50b85685b359f176ecb905dc61d
Michael McCandless <lucene@mikemccandless.com>
2011-08-27 15:23:44 -0400

    cutover to FieldType

--- perf/PKLookupPerfTest.java
+++ perf/PKLookupPerfTest.java
@@ -63,8 +63,7 @@ public class PKLookupPerfTest {
     w.setInfoStream(System.out);
    
     final Document doc = new Document();
-    final Field field = new Field("id", "", Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS);
-    field.setIndexOptions(FieldInfo.IndexOptions.DOCS_ONLY);
+    final Field field = new Field("id", StringField.TYPE_STORED, "");
     doc.add(field);
 
     for(int i=0;i<docCount;i++) {

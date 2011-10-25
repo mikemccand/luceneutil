c201423aab87248a692dc6ce6e2cb796a4eb67e4
Michael McCandless <lucene@mikemccandless.com>
2011-10-25 06:11:09 -0400

    add lang detect eval sources

---
+++ langdetect/LDDetectLanguagePerfTest.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import com.cybozu.labs.langdetect.DetectorFactory;
+import com.cybozu.labs.langdetect.Detector;
+import com.cybozu.labs.langdetect.Language;
+import java.util.*;
+
+// javac -cp /home/mike/src/langdetect/lib/langdetect.jar /lucene/util/langdetect/LDDetectLanguagePerfTest.java 
+
+// java -cp /home/mike/src/langdetect/lib/langdetect.jar:/home/mike/src/langdetect/lib/jsonic-1.2.0.jar:/lucene/util/langdetect LDDetectLanguagePerfTest /lucene/util/europarl.17.test 
+
+public class LDDetectLanguagePerfTest {
+    private static final double DEFAULT_ALPHA = 0.5;
+
+    public static void main(String[] args) throws Exception {
+        List<String> testData = new ArrayList<String>();
+        BufferedReader is = new BufferedReader(new InputStreamReader(new FileInputStream(args[0]), "utf-8"));
+        int totBytes = 0;
+        while (true) {
+            String line = is.readLine();
+            if (line == null) {
+                break;
+            }
+            line = line.trim();
+            int idx = line.indexOf('\t');
+            if (idx <= 0) continue;
+            String text = line.substring(idx + 1);
+            testData.add(text);
+            totBytes += text.length();
+        }
+        is.close();
+
+        DetectorFactory.loadProfile("/home/mike/src/langdetect/profiles");
+        DetectorFactory.setSeed(0);
+
+        long best = -1;
+        for(int iter=0;iter<10;iter++) {
+            System.out.println("iter " + iter);
+            List<String> answers = new ArrayList<String>();
+            long t0 = System.currentTimeMillis();
+            for(String test : testData) {
+                final Detector d = DetectorFactory.create(DEFAULT_ALPHA);
+                d.append(test);
+                String answer = d.detect();
+                answers.add(d.detect());
+            }
+            long t = System.currentTimeMillis() - t0;
+            System.out.println("  " + t + " msec");
+            if (best == -1 || t < best) {
+                best = t;
+                System.out.println("  **");
+            }
+        }
+        System.out.println("Best " + best + " msec; totBytes=" + totBytes + " MB/sec=" + (totBytes/1024./1024./(best/1000.)));
+    }
+}

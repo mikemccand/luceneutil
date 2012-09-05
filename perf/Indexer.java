9f6905e593c40efc4a6131f81db0642cd08c59b6
Michael McCandless <lucene@mikemccandless.com>
2012-09-06 01:18:29 +0200

    disable bloom

--- perf/Indexer.java
+++ perf/Indexer.java
@@ -36,8 +36,8 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.util.CharArraySet;
 import org.apache.lucene.codecs.Codec;
 import org.apache.lucene.codecs.PostingsFormat;
-import org.apache.lucene.codecs.bloom.BloomFilteringPostingsFormat;
-import org.apache.lucene.codecs.bloom.DefaultBloomFilterFactory;
+//import org.apache.lucene.codecs.bloom.BloomFilteringPostingsFormat;
+//import org.apache.lucene.codecs.bloom.DefaultBloomFilterFactory;
 import org.apache.lucene.codecs.lucene40.Lucene40Codec;
 import org.apache.lucene.document.*;
 import org.apache.lucene.index.*;
@@ -187,6 +187,7 @@ public final class Indexer {
     final Codec codec = new Lucene40Codec() {
       @Override
       public PostingsFormat getPostingsFormatForField(String field) {
+	  /*
         if (field.equals("id")) {
           if (idFieldPostingsFormat.equals("BloomLucene40")) {
             return new BloomFilteringPostingsFormat(
@@ -198,7 +199,7 @@ public final class Indexer {
                      new DefaultBloomFilterFactory());
           }
         }
-            
+	  */
         return PostingsFormat.forName(field.equals("id") ?
                                       idFieldPostingsFormat : defaultPostingsFormat);
       }

4d45527310c70df43bb1473ebac324d0863885e6
Michael McCandless <lucene@mikemccandless.com>
2013-02-20 16:02:46 -0500

    test passes again

--- mergeFacets.py
+++ mergeFacets.py
@@ -1,6 +1,14 @@
 import sys
 import random
 
+# TODO
+#   - only pass necessary values out to each shard on re-iter
+#   - don't increase topN unless we have to
+#   - try different facet cases
+#     - the adversary
+#     - draws-from-same-model
+#     - fully orthogonal
+
 class FacetShard:
 
   def __init__(self, r, facetValues):
@@ -18,20 +26,23 @@ class FacetShard:
   def getHits(self, topN=None, specificValues=None):
     if topN is None:
       assert specificValues is None
-      return self.results, None
+      return self.results, None, None
     else:
       l = self.results[:topN]
       if specificValues is not None:
+        l2 = []
         seen = set()
-        for value, count in self.results[topN:]:
+        for value, count in self.results:
           if value in specificValues:
             seen.add(value)
-            l.append((value, count))
+            l2.append((value, count))
         for value in specificValues:
           if value not in seen:
-            l.append((value, 0))
+            l2.append((value, 0))
+      else:
+        l2 = None
           
-      return l, len(self.results)
+      return l, len(self.results), l2
 
 def randomString(r):
   len = r.randint(1, 10)
@@ -55,9 +66,13 @@ def merge(shards, topN):
   # holds (truncated, map<value,count>, and lowestCount) for each shard:
   shardHits = [(False, None, None)] * len(shards)
 
+  iterCount = 0
+
   while True:
     if VERBOSE:
       print '    cycle mult=%s' % mult
+
+    iterCount += 1
 
     if lTopN is not None:
       specificValues = [x[0] for x in lTopN]
@@ -68,25 +83,50 @@ def merge(shards, topN):
       print '    query shards'
     for i in xrange(len(shards)):
       exhausted, shardValues, lowestCount = shardHits[i]
+      if specificValues is not None:
+        shardMissingValues = []
+        for label in specificValues:
+          if label is not None and label not in shardValues:
+            shardMissingValues.append(label)
+      else:
+        shardMissingValues = None
+        
       if not exhausted:
         # nocommit make this a searchAfter ... ie if we already got the
         # first 10, then don't get them again
-        hits, totalHitCount = shards[i].getHits(mult*topN, specificValues)
+        hits, totalHitCount, newValues = shards[i].getHits(mult*topN, shardMissingValues)
         exhausted = totalHitCount <= mult*topN
-        shardValues = {}
 
-        lowestCount = 0
-        for j, (value, count) in enumerate(hits):
+        if shardValues is None:
+          shardValues = {}
+            
+        for value, count in hits:
+          # nocommit re-enable:
+          #assert value not in shardValues
           shardValues[value] = count
-          if j == topN-1:
-            lowestCount = count
+
+        if exhausted:
+          lowestCount = 0
+        else:
+          lowestCount = hits[-1][1]
+
+        if newValues is not None:
+          for value, count in newValues:
+            shardValues[value] = count
 
         if VERBOSE:
-          print '      shard %d: totalHits=%s len(hits)=%s exhausted=%s lowestCount=%d' % \
-                (i, totalHitCount, len(hits), exhausted, lowestCount)
+          print '      shard %d: totalHits=%s len(hits)=%s exhausted=%s lowestCount=%d values=%s' % \
+                (i, totalHitCount, len(hits), exhausted, lowestCount, shardMissingValues)
           for value, count in hits:
-            print '        %s: count=%s' % (value, lowestCount)
+            print '        %s: count=%s' % (value, count)
+          if newValues is not None:
+            for value, count in newValues:
+              print '       *%s: count=%d' % (value, count)
         shardHits[i] = (exhausted, shardValues, lowestCount)
+      else:
+        if VERBOSE:
+          print '      shard %d: skip exhausted=%s len(shardMissingValues)=%s' % \
+                (i, exhausted, len(shardMissingValues))
 
     # nocommit must handle the "all shards have 0 facets" case ... we
     # will exc below
@@ -108,10 +148,13 @@ def merge(shards, topN):
     # Second pass merge: add in proxy counts for values missing from
     # shards, to see if they might penetrate the requested topN:
     for value, l in merged.items():
+      #print '  merged value %s' % value
       for shard, (exhausted, shardValues, lowestCount) in enumerate(shardHits):
+        #print '    shard %d, exhaused %s, values %s' % (shard, exhausted, shardValues)
         if not exhausted and value not in shardValues:
           #if VERBOSE:
           #  print '      shard %d missing value %s' % (shard, value)
+          # nocommit this could be "smarter", eg a pro-rated estimate:
           l[0] += lowestCount
           l[1] = True
 
@@ -122,7 +165,6 @@ def merge(shards, topN):
     if sumLowestCount > 0:
       # nocommit how come test doesn't fail if i comment this out!
       l.append((None, [sumLowestCount, True]))
-      pass
     
     l.sort(cmpByCountThenLabel2)
 
@@ -153,13 +195,15 @@ def merge(shards, topN):
       # nocommit sometimes ... we don't need to increase mult, ie, we
       # only need to request certain values
       # nocommit why True or needed...
-      if True or sawNone:
+      if sawNone:
         mult *= 2
       if VERBOSE:
         print '  run again with mult=%s' % mult
       continue
     else:
       break
+
+  print '    %d iters' % iterCount
 
   # l is list of (value, ([shardSeenCount, totalCount])), sorted by totalCount descending
   return [(x[0], x[1][0]) for x in l[:topN]]
@@ -180,6 +224,7 @@ def test(seedIn):
 
     facetValues = set()
     numFacetValues = r.randint(10, 100)
+    #numFacetValues = 10
     while len(facetValues) < numFacetValues:
       facetValues.add(randomString(r))
     facetValues = list(facetValues)
@@ -215,7 +260,7 @@ def test(seedIn):
       # nocommit
       #topN = r.randint(1, 1000)
       #topN = 2
-      if VERBOSE:
+      if True or VERBOSE:
         print '  iter: topN=%d' % topN
 
       expected = allResults[:topN]

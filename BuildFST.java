d010368d3e8b9231c828499707adb77d983a9d88
Michael McCandless <lucene@mikemccandless.com>
2013-06-09 08:41:48 -0400

    improve fst builder

--- BuildFST.java
+++ BuildFST.java
@@ -24,7 +24,13 @@ import org.apache.lucene.util.IntsRef;
 import org.apache.lucene.util.fst.Builder;
 import org.apache.lucene.util.fst.ByteSequenceOutputs;
 import org.apache.lucene.util.fst.FST;
+import org.apache.lucene.util.fst.Outputs;
+import org.apache.lucene.util.fst.PositiveIntOutputs;
 import org.apache.lucene.util.fst.Util;
+
+// TODO
+//  - test 0 inputs
+//  - blog post: FSA, numeric, non-numeric, with generics violations
 
 // pushd ../buildfst/lucene/core; ant jar; popd; javac -cp /l/buildfst/lucene/build/core/lucene-core-4.4-SNAPSHOT.jar BuildFST.java; java -cp .:/l/buildfst/lucene/build/core/lucene-core-4.4-SNAPSHOT.jar BuildFST foo/bar feo/baz > /x/tmp/out.dot; dot -Tpng /x/tmp/out.dot > /x/tmp/out.png
 
@@ -32,11 +38,11 @@ import org.apache.lucene.util.fst.Util;
 
 public class BuildFST {
 
-  static class Pair implements Comparable<Pair> {
+  static class Pair<T> implements Comparable<Pair<T>> {
     BytesRef input;
-    BytesRef output;
+    T output;
 
-    public Pair(BytesRef input, BytesRef output) {
+    public Pair(BytesRef input, T output) {
       this.input = input;
       this.output = output;
     }
@@ -48,22 +54,64 @@ public class BuildFST {
   }
 
   public static void main(String[] args) throws IOException {
-    Pair[] inputs = new Pair[args.length];
+
+    boolean numeric = true;
+    boolean negative = false;
     for(int i=0;i<args.length;i++) {
       String[] pair = args[i].split("/");
-      if (pair.length != 2) {
+      if (pair.length > 2) {
+        throw new RuntimeException("each arg should be input or input/output pair");
+      }
+      if (pair.length == 2) {
+        try {
+          negative |= Long.parseLong(pair[1]) < 0;
+        } catch (NumberFormatException nfe) {
+          numeric = false;
+          break;
+        }
+      }
+    }
+
+    Outputs outputs;
+    if (numeric) {
+      if (negative) {
+        throw new RuntimeException("can only handle numeric outputs >= 0");
+      }
+      outputs = PositiveIntOutputs.getSingleton();
+    } else {
+      outputs = ByteSequenceOutputs.getSingleton();
+    }
+    
+    Pair<?>[] inputs = new Pair[args.length];
+    for(int i=0;i<args.length;i++) {
+      String[] pair = args[i].split("/");
+      if (pair.length > 2) {
         throw new RuntimeException("each arg should be input/output pair");
       }
-      inputs[i] = new Pair(new BytesRef(pair[0]), new BytesRef(pair[1]));
+      Object output;
+      if (pair.length == 1) {
+        output = outputs.getNoOutput();
+      } else {
+        output = Long.parseLong(pair[1]);
+      }
+      inputs[i] = new Pair(new BytesRef(pair[0]), output);
     }
     Arrays.sort(inputs);
 
-    final ByteSequenceOutputs outputs = ByteSequenceOutputs.getSingleton();
-    Builder<BytesRef> b = new Builder<BytesRef>(FST.INPUT_TYPE.BYTE1, outputs);
-    for(Pair pair : inputs) {
-      b.add(Util.toIntsRef(pair.input, new IntsRef()), pair.output);
+    FST<?> fst;
+    if (numeric) {
+      Builder<Long> b = new Builder<Long>(FST.INPUT_TYPE.BYTE1, outputs);
+      for(Pair pair : inputs) {
+        b.add(Util.toIntsRef(pair.input, new IntsRef()), (Long) pair.output);
+      }
+      fst = b.finish();
+    } else {
+      Builder<BytesRef> b = new Builder<BytesRef>(FST.INPUT_TYPE.BYTE1, outputs);
+      for(Pair pair : inputs) {
+        b.add(Util.toIntsRef(pair.input, new IntsRef()), (BytesRef) pair.output);
+      }
+      fst = b.finish();
     }
-    FST<BytesRef> fst = b.finish();
     Util.toDot(fst, new PrintWriter(System.out), true, true);
   }
 }
--- fstApp.py
+++ fstApp.py
@@ -5,6 +5,8 @@ import wsgiref.simple_server
 
 LUCENE_JAR = '/l/buildfst/lucene/build/core/lucene-core-4.4-SNAPSHOT.jar'
 
+# bad: http://10.17.4.91:11000/fst?terms=mop%2F1%0D%0Amoth%0D%0Apop%2F4%0D%0Astar%0D%0Astop%0D%0Atop%2F3%0D%0A%0D%0A&cmd=Build+FST
+
 def application(environ, startResponse):
   _l = []
   w = _l.append
@@ -16,7 +18,7 @@ def application(environ, startResponse):
   #print('environ: %s' % str(environ))
 
   args = cgi.parse(environ=environ)
-  print('GOT ARGS: %s' % str(args))
+  #print('GOT ARGS: %s' % str(args))
   if 'terms' in args:
     terms = args['terms'][0]
   else:
@@ -24,14 +26,39 @@ def application(environ, startResponse):
 
   w('<html>')
   w('<body>')
+  w('<h2>Build your own FST</h2>')
+  if terms is None:
+    terms = '''mop/1
+moth/2
+pop/3
+star/4
+stop/5
+top/6
+'''
+
+  w('<table>')
+  w('<tr>')
+  w('<td valign=top>')
   w('<form method=GET action="/fst">')
   w('<textarea name="terms" cols=50 rows=10>')
   if terms is not None:
     w(terms)
   w('</textarea>')
   w('<br>')
-  w('<input type=submit name=cmd value="Build FST">')
+  w('<input type=submit name=cmd value="Build it!">')
   w('</form>')
+  w('</td>')
+  w('<td valign=top>')
+  w('<ul>')
+  w('<li> Each entry can be input (creates an FSA) or input/ouput (creates an FST).')
+  w('<li> Separate each entry with space or newline.')
+  w('<li> If all outputs are ints > 0 then outputs are numeric (sum as you traverse); otherwise outputs are strings (concatenate as you traverse).')
+  w('<li> NEXT-optimized arcs (whose target is the next node) are <font color=red>red</font>.')
+  w('<li> A bolded arc means the next node is final.')
+  w('<li> See <a href="http://blog.mikemccandless.com">this blog post</a> for details and examples.')
+  w('</ul>')
+  w('</tr>')
+  w('</table>')
 
   if terms is not None:
 
@@ -39,8 +66,8 @@ def application(environ, startResponse):
     error = None
     for x in l:
       tup = x.split('/')
-      if len(tup) != 2:
-        error = 'Each item should be input/output string; got invalid item: %s' % x
+      if len(tup) > 2:
+        error = 'Each item should be input or input/output string; got invalid item: %s' % x
         break
       
     if error is not None:
@@ -49,19 +76,20 @@ def application(environ, startResponse):
     p = subprocess.Popen(['java', '-cp', '.:%s' % LUCENE_JAR, 'BuildFST'] + l, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     dotString, err = p.communicate()
     if p.returncode != 0:
+      w('<br>')
       w('Sorry, BuildFST failed:<br>')
-      w('<pre>%s</pre>' % err)
+      w('<pre>%s</pre>' % err.decode('utf-8'))
     else:
-      print('got dot: %s' % dotString)
+      #print('got dot: %s' % dotString.decode('ascii'))
       p = subprocess.Popen(['dot', '-Tpng'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
       pngData, err = p.communicate(dotString)
       if p.returncode != 0:
+        w('<br>')
         w('Sorry, dot failed:<br>')
-        w('<pre>%s</pre>' % err)
+        w('<pre>%s</pre>' % err.decode('utf-8'))
       else:
         w('<br><br>')
         w('<img src="data:image/png;base64,%s">' % base64.b64encode(pngData).decode('ascii'))
-
   w('</body>')
   w('</html>')
 

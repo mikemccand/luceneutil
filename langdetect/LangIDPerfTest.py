c93c516a0c1343a2750ecd8d09b35873213627b3
Michael McCandless <lucene@mikemccandless.com>
2013-06-08 20:03:46 -0400

    count bytes unicode vs utf8

--- langdetect/LangIDPerfTest.py
+++ langdetect/LangIDPerfTest.py
@@ -4,7 +4,8 @@ import sys
 import io
 
 testData = []
-totBytes = 0
+totBytesUnicode = 0
+totBytesUTF8 = 0
 f = io.open(sys.argv[1], 'r', encoding='utf-8')
 while True:
   line = f.readline()
@@ -14,17 +15,20 @@ while True:
   if idx == -1:
     continue
   test = line[idx+1:].strip()
-  totBytes += len(test)
-  testData.append(test.encode('utf-8'))
+  totBytesUnicode += len(test)
+  utf8 = test.encode('utf-8')
+  totBytesUTF8 += len(utf8)
+  testData.append(utf8)
 
-print 'Tot bytes %s' % totBytes
+print 'Tot bytes %s vs %s' % (totBytesUnicode, totBytesUTF8)
 
 best = -1
 for i in range(10):
   answers = []
   t0 = time.time()
   for test in testData:
-    answers.append(langid.classify(test)[0])
+    #answers.append(langid.classify(test)[0])
+    langid.classify(test)
   t = time.time() - t0
   print '%.1f msec' % (1000*t)
   if best == -1 or t < best:

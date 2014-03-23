1c29fc32e4073b1199842711c5741ee3fbfb384a
serera <devnull@localhost>
2014-03-23 21:48:09 +0200

    add reopenStats to NRTPerfTest.py

--- src/python/NRTPerfTest.py
+++ src/python/NRTPerfTest.py
@@ -4,6 +4,8 @@ import constants
 import re
 import benchUtil
 from competition import *
+import stats
+from __builtin__ import len
 
 def run(command):
   if os.system(command):
@@ -32,11 +34,16 @@ def main():
   # fix reopen rate, vary indexing rate
   #for indexRate in (100, 200, 500, 1000, 2000, 5000, 10000):
   indexRate = 2000
-  for reopenRate in (0.1, 0.5, 1.0, 5.0, 10.0, 20.0):
+#  for reopenRate in (0.1, 0.5, 1.0, 5.0, 10.0, 20.0):
+  for reopenRate in (0.1, 0.5, 1.0):
     logFileName = '%s/dps%s_reopen%s.txt' % (constants.LOGS_DIR, indexRate, reopenRate)
     docCount, searchCount, readerCount, runTimeSec = runOne(cp, indexRate, reopenRate, logFileName)
+    reopenStats(logFileName)
     print 'Index rate target=%s/sec: %.2f docs/sec; %.2f reopens/sec; %.2f searches/sec' % (indexRate, docCount/float(runTimeSec), readerCount/float(runTimeSec), searchCount/float(runTimeSec))
-  
+
+reNRTReopenTime = re.compile('^Reopen: +([0-9.]+) msec$', re.MULTILINE)
+reByTime = re.compile('  (\d+) searches=(\d+) docs=(\d+) reopens=(\d+)$')
+
 def runOne(claspath, docsPerSec, reopensPerSec, logFileName):
   if ADDS_ONLY:
     mode = 'add'
@@ -70,7 +77,6 @@ def runOne(claspath, docsPerSec, reopensPerSec, logFileName):
 
   try:
     perTimeQ = []
-    reByTime = re.compile('  (\d+) searches=(\d+) docs=(\d+) reopens=(\d+)$')
     qtCount = 0
     totSearches = 0
     totDocs = 0
@@ -89,12 +95,32 @@ def runOne(claspath, docsPerSec, reopensPerSec, logFileName):
           totDocs += docs
           totReopens += reopens
           qtCount += 1
-    
+  
     return totDocs, totSearches, totReopens, qtCount * STATS_EVERY_SEC
   except:
     print 'FAILED -- output:\n%s' % result
     raise
 
+def reopenStats(logFileName):
+  times = []
+  for line in open(logFileName, 'rb').read().split('\n'):
+    m = reNRTReopenTime.match(line.rstrip())
+    if m is not None:
+      times.append(float(m.group(1)))
+    
+  # Discard first 10% (JVM warmup): minimum of 1 but no more than 10
+  times = times[min(10, max(1, len(times) / 10)):]
+
+  # Discard worst 2%
+  times.sort()
+  numDrop = len(times)/50
+  if numDrop > 0:
+    print 'drop: %s' % ' '.join(['%.1f' % x for x in times[-numDrop:]])
+    times = times[:-numDrop]
+  print 'times: %s' % ' '.join(['%.1f' % x for x in times])
+
+  minVal, maxVal, mean, stdDev = stats.getStats(times)
+  print 'NRT reopen time (msec) mean=%.4f stdDev=%.4f' % (mean, stdDev)
   
 if __name__ == '__main__':
   main()

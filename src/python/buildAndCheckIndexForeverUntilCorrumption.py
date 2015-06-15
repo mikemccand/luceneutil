a871bcdbf3831885e2d4831d94146304dc542ada
Michael McCandless <lucene@mikemccandless.com>
2015-06-15 15:37:48 -0400

    pull out line docs file source

--- src/python/buildAndCheckIndexForeverUntilCorrumption.py
+++ src/python/buildAndCheckIndexForeverUntilCorrumption.py
@@ -13,6 +13,9 @@ LUCENE_TRUNK_ROOT = 'trunk.nightly'
 # Where to write indices:
 INDEX_PATH = '/q/lucene/indices/corruption'
 
+# Docs to index:
+LINE_DOCS_FILE = '/lucene/data/enwiki-20110115-lines-1k-fixed.txt'
+
 # Customize the java command-line:
 JAVA_CMD = 'java'
 
@@ -30,7 +33,7 @@ while True:
 
   shutil.rmtree(INDEX_PATH)
 
-  if os.system('java -classpath "ROOT/lucene/build/core/classes/java:ROOT/lucene/build/core/classes/test:ROOT/lucene/build/sandbox/classes/java:ROOT/lucene/build/misc/classes/java:ROOT/lucene/build/facet/classes/java:/home/mike/src/lucene-c-boost/dist/luceneCBoost-SNAPSHOT.jar:ROOT/lucene/build/analysis/common/classes/java:ROOT/lucene/build/analysis/icu/classes/java:ROOT/lucene/build/queryparser/classes/java:ROOT/lucene/build/grouping/classes/java:ROOT/lucene/build/suggest/classes/java:ROOT/lucene/build/highlighter/classes/java:ROOT/lucene/build/codecs/classes/java:ROOT/lucene/build/queries/classes/java:lib/HdrHistogram.jar:build" perf.Indexer -dirImpl MMapDirectory -indexPath "%s" -analyzer StandardAnalyzerNoStopWords -lineDocsFile /lucene/data/enwiki-20110115-lines-1k-fixed.txt -docCountLimit %s -threadCount 1 -maxConcurrentMerges 3 -dvfields -ramBufferMB -1 -maxBufferedDocs 49774 -postingsFormat Lucene50 -waitForMerges -mergePolicy LogDocMergePolicy -facets Date -facetDVFormat Lucene50 -idFieldPostingsFormat Memory'.replace('ROOT', '%s/%s' % (constants.BASE_DIR, LUCENE_TRUNK_ROOT)) % (INDEX_PATH, DOC_COUNT)):
+  if os.system('java -classpath "ROOT/lucene/build/core/classes/java:ROOT/lucene/build/core/classes/test:ROOT/lucene/build/sandbox/classes/java:ROOT/lucene/build/misc/classes/java:ROOT/lucene/build/facet/classes/java:/home/mike/src/lucene-c-boost/dist/luceneCBoost-SNAPSHOT.jar:ROOT/lucene/build/analysis/common/classes/java:ROOT/lucene/build/analysis/icu/classes/java:ROOT/lucene/build/queryparser/classes/java:ROOT/lucene/build/grouping/classes/java:ROOT/lucene/build/suggest/classes/java:ROOT/lucene/build/highlighter/classes/java:ROOT/lucene/build/codecs/classes/java:ROOT/lucene/build/queries/classes/java:lib/HdrHistogram.jar:build" perf.Indexer -dirImpl MMapDirectory -indexPath "%s" -analyzer StandardAnalyzerNoStopWords -lineDocsFile %s -docCountLimit %s -threadCount 1 -maxConcurrentMerges 3 -dvfields -ramBufferMB -1 -maxBufferedDocs 49774 -postingsFormat Lucene50 -waitForMerges -mergePolicy LogDocMergePolicy -facets Date -facetDVFormat Lucene50 -idFieldPostingsFormat Memory'.replace('ROOT', '%s/%s' % (constants.BASE_DIR, LUCENE_TRUNK_ROOT)) % (INDEX_PATH, LINE_DOCS_FILE, DOC_COUNT)):
     raise RuntimeError('failed to build index')
 
   print('%s: check index' % datetime.datetime.now())

87d564edcdda80ebb3867e7522368d73307a85f1
Mike McCandless <mikemccand@apache.org>
2016-10-31 20:20:32 -0400

    make back tester work by commit times, not date

--- src/python/sparsetaxis/backTest.py
+++ src/python/sparsetaxis/backTest.py
@@ -34,120 +34,89 @@ with os.popen('git log --format=fuller', 'r') as f:
         commitTime = datetime.datetime.strptime(s, '%a %b %d %H:%M:%S %Y %z')
         #if len(commits) > 0 and commitTime > commits[-1][1]:
         #  print('wrong order: %s -> %s vs %s, %s' % (s, commitTime, commits[-1][1], commits[-1]))
-        commits.append((commitHash, commitTime))
+        commits.append((commitHash, datetime.datetime(*commitTime.utctimetuple()[:6])))
         #print('got: %s, %s' % (commitHash, commitTime))
 
-dateStart = datetime.date(2016, 9, 20)
-dateEnd = datetime.date(2016, 10, 30)
+dateStart = datetime.datetime(2016, 9, 15)
 
 upto = 0
 lastTestedDate = None
-dateToCommitIndex = {}
+timeToCommitIndex = {}
 while True:
-
-  # Go back to last commit the day before:
-  if lastTestedDate is not None:
-    while True:
-      hash, commitTime = commits[upto]
-      utc = commitTime.utctimetuple()
-      date = commitTime.date()
-      if date != lastTestedDate:
-        break
-      upto += 1
 
   hash, commitTimeStamp = commits[upto]
 
-  utc = commitTimeStamp.utctimetuple()
+  timestamp = datetime.datetime(*commitTimeStamp.utctimetuple()[:6])
 
-  lastTestedDate = datetime.date(year=utc[0], month=utc[1], day=utc[2])
-
-  print('%s -> %s' % (lastTestedDate, upto))
-  dateToCommitIndex[lastTestedDate] = upto
+  print('%s -> %s' % (timestamp, upto))
+  timeToCommitIndex[timestamp] = upto
   upto += 1
 
-  if lastTestedDate < dateStart:
+  if timestamp < dateStart:
+    del commits[upto:]
     break
 
-datesAlreadyDone = set()
+timesAlreadyDone = set()
 for name in os.listdir(TAXIS_LOGS_DIR):
   if os.path.isdir('%s/%s' % (TAXIS_LOGS_DIR, name)):
     if os.path.exists('%s/%s/results.pk' % (TAXIS_LOGS_DIR, name)):
-      tup = list(int(x) for x in name.split('.')[:3])
-      date = datetime.date(year=tup[0], month=tup[1], day=tup[2])
-      print('already done: %s' % date)
-      datesAlreadyDone.add(date)
+      tup = list(int(x) for x in name.split('.'))
+      timestamp = datetime.datetime(*tup)
+      print('already done: %s' % timestamp)
+      timesAlreadyDone.add(timestamp)
 
-oneDay = datetime.timedelta(days=1)
- 
-def getDatesToTest():
+def getTimesToTest():
+
   """
-  Carefully enumerates dates in such a way that we test large gaps first, then smaller
-  gaps, and eventually all dates in range.
+  Carefully enumerates commit timestamps in such a way that we test large gaps first, then smaller
+  gaps, and eventually all commits in the range.
   """
-
-  if dateStart not in datesAlreadyDone:
-    yield dateStart
-
-  if dateEnd not in datesAlreadyDone:
-    yield dateEnd
-
-  totalMaxMinDistance = None
-  totalMaxMinDate = None
 
   while True:
-    date = dateStart
-    while date <= dateEnd:
-      date += oneDay
-      if date in datesAlreadyDone:
+
+    totalMaxMinTimestamp = None
+    totalMaxMinDistance = None
+    for hash, timestamp in commits:
+
+      if timestamp in timesAlreadyDone:
         continue
 
-      # consider choosing date next
-
-      # find the closest date already done, to this candidate date:
+      # consider choosing timestamp; find the closest timestamp already done, to this candidate:
       minDistance = None
-      for date2 in datesAlreadyDone:
-        distance = abs((date - date2).total_seconds())
+      for timestamp2 in timesAlreadyDone:
+        distance = abs((timestamp - timestamp2).total_seconds())
         if minDistance is None or distance < minDistance:
           minDistance = distance
 
       if totalMaxMinDistance is None or minDistance > totalMaxMinDistance:
         totalMaxMinDistance = minDistance
-        totalMaxMinDate = date
+        totalMaxMinTimestamp = timestamp
 
-    if totalMaxMinDate is None:
+    if totalMaxMinTimestamp is None:
       break
 
-    yield totalMaxMinDate
+    yield totalMaxMinTimestamp
           
 
 lastTestedDate = None
 
-for date in getDatesToTest():
+for timestamp in getTimesToTest():
 
-  if date not in dateToCommitIndex:
-    # no commits on this date:
-    datesAlreadyDone.add(date)
-    print('  skip date %s: no commits' % date)
-    continue
-
-  hash, commitTimeStamp = commits[dateToCommitIndex[date]]
+  hash, commitTimeStamp = commits[timeToCommitIndex[timestamp]]
 
   utc = commitTimeStamp.utctimetuple()
 
   ts = '%04d.%02d.%02d.%02d.%02d.%02d' % utc[:6]
   
   print('\n%s TEST: %s, %s' % (datetime.datetime.now(), hash, commitTimeStamp))
-  if date in datesAlreadyDone:
-    print('  already done: %s' % date)
-  else:
-    datesAlreadyDone.add(date)
-    logDir = '/l/logs.nightly/taxis/%s' % ts
-    if os.path.exists(logDir):
-      print('  remove old partial log dir %s' % logDir)
-      shutil.rmtree(logDir)
-      
-    run('git checkout %s > /dev/null 2>&1' % hash)
-    run('git clean -xfd')
-    run('python3 -u /l/util/src/python/sparsetaxis/runBenchmark.py -rootDir /l/sparseTaxis -logDir %s -luceneMaster /l/sparseTaxis/sparseTaxis/lucene.master' % logDir)
-    run('python3 -u /l/util/src/python/sparsetaxis/writeGraph.py')
+  timesAlreadyDone.add(timestamp)
+  logDir = '/l/logs.nightly/taxis/%s' % ts
+  if os.path.exists(logDir):
+    print('  remove old partial log dir %s' % logDir)
+    shutil.rmtree(logDir)
+
+  run('git checkout %s > /dev/null 2>&1' % hash)
+  run('git clean -xfd')
+  run('python3 -u /l/util/src/python/sparsetaxis/runBenchmark.py -rootDir /l/sparseTaxis -logDir %s -luceneMaster /l/sparseTaxis/sparseTaxis/lucene.master' % logDir)
+  run('python3 -u /l/util/src/python/sparsetaxis/writeGraph.py')
 

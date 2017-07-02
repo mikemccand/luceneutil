ba764a4f71a46564d6a78b5b47c8459095493c69
Mike McCandless <mikemccand@apache.org>
2017-07-02 08:07:13 -0400

    use nightly area to run sparse taxi benchmarks

--- src/python/sparsetaxis/nightly.py
+++ src/python/sparsetaxis/nightly.py
@@ -3,14 +3,14 @@ import datetime
 
 now = datetime.datetime.now()
 
-os.chdir('/l/sparseTaxis/sparseTaxis/lucene.master/')
+os.chdir('/l/sparseTaxis.nightly/sparseTaxis/lucene.master')
 if os.system('git checkout master'):
   raise RuntimeError('git checkout master failed')
 
 if os.system('git pull origin master'):
   raise RuntimeError('git pull failed')
 
-if os.system('python3 -u /l/util.nightly/src/python/sparsetaxis/runBenchmark.py -rootDir /l/sparseTaxis -logDir /l/logs.nightly/taxis/%4d.%02d.%02d.%02d.%02d.%02d -luceneMaster /l/sparseTaxis/sparseTaxis/lucene.master' % \
+if os.system('python3 -u /l/util.nightly/src/python/sparsetaxis/runBenchmark.py -rootDir /l/sparseTaxis.nightly -logDir /l/logs.nightly/taxis/%4d.%02d.%02d.%02d.%02d.%02d -luceneMaster /l/sparseTaxis.nightly/sparseTaxis/lucene.master' % \
              (now.year, now.month, now.day, now.hour, now.minute, now.second)):
   raise RuntimeError('bench failed')
 

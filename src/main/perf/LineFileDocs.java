4385e548414b07ba911b69a99bc5cc746cb8431a
Mayya Sharipova <mayya.sharipova@elastic.co>
2020-06-23 15:14:44 -0400

    Index points to benchmark sort optimization
    Sort optimization introduced in https://github.com/apache/lucene-solr/pull/1351
    depends on numeric fields being indexed both as doc_values and points.
    
    This PR does the following:
    - add a LongPoint field – lastModLP, last modified timestamp
    - add an IntPoint field – dayOfYearIP, day of the year of the last modified timestamp
    - add sort on the last modified timestamp to wikimedium.10M.nostopwords.tasks
    - don't fail a task if hitCounts don't match in benchUtil.py. As we
    don't collect all hits in the optimized runs, we don't expect hits total
    to match.

--- src/main/perf/LineFileDocs.java
+++ src/main/perf/LineFileDocs.java
@@ -1,3 +1,4 @@
+
 package perf;
 
 /**
@@ -299,8 +300,10 @@ public class LineFileDocs implements Closeable {
     final Field titleDV;
     final Field monthDV;
     final Field dayOfYearDV;
+    final IntPoint dayOfYearIP;
     final BinaryDocValuesField titleBDV;
     final NumericDocValuesField lastModNDV; 
+    final LongPoint lastModLP;
     final Field body;
     final Field id;
     final Field idPoint;
@@ -333,18 +336,24 @@ public class LineFileDocs implements Closeable {
 
       	lastModNDV = new NumericDocValuesField("lastModNDV", -1);
       	doc.add(lastModNDV);
+	lastModLP = new LongPoint("lastModNDV", -1); //points field must have the same name and value as DV field
+	doc.add(lastModLP);
 
         monthDV = new SortedDocValuesField("monthSortedDV", new BytesRef(""));
       	doc.add(monthDV);
-        
-        dayOfYearDV = new NumericDocValuesField("dayOfYearNumericDV", 0);
-      	doc.add(dayOfYearDV);
+	
+	dayOfYearDV = new NumericDocValuesField("dayOfYearNumericDV", 0);
+	doc.add(dayOfYearDV);
+	dayOfYearIP = new IntPoint("dayOfYearNumericDV", 0); //points field must have the same name and value as DV field
+	doc.add(dayOfYearIP);
       } else {
       	titleDV = null;
         titleBDV = null;
       	lastModNDV = null;
+	lastModLP = null;
         monthDV = null;
         dayOfYearDV = null;
+	dayOfYearIP = null;
       }
       
       titleTokenized = new Field("titleTokenized", "", TextField.TYPE_STORED);
@@ -533,6 +542,7 @@ public class LineFileDocs implements Closeable {
       doc.titleTokenized.setStringValue(title);
       doc.monthDV.setBytesValue(new BytesRef(months[doc.dateCal.get(Calendar.MONTH)]));
       doc.dayOfYearDV.setLongValue(doc.dateCal.get(Calendar.DAY_OF_YEAR));
+      doc.dayOfYearIP.setIntValue(doc.dateCal.get(Calendar.DAY_OF_YEAR));
     }
     doc.id.setStringValue(intToID(myID));
 
@@ -540,6 +550,7 @@ public class LineFileDocs implements Closeable {
 
     if (addDVFields) {
       doc.lastModNDV.setLongValue(msecSinceEpoch);
+      doc.lastModLP.setLongValue(msecSinceEpoch);
     }
 
     doc.timeSec.setIntValue(timeSec);

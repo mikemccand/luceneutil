8a67c0c165afb6e658e7e3855ab6c2abbcad11a2
Mike McCandless <mikemccand@apache.org>
2021-01-10 06:46:51 -0500

    small improvements to wiki line docs tooling

--- src/python/splitWikiLineFile.py
+++ src/python/splitWikiLineFile.py
@@ -1,21 +1,28 @@
 import sys
 
-f = open(sys.argv[1], 'rb')
-fOut = open(sys.argv[2], 'wb')
+f = open(sys.argv[1], 'r')
+fOut = open(sys.argv[2], 'w')
 numCharLimit = int(sys.argv[3])
 
+# copy header over
+l = f.readline()
+fOut.write(l)
+
 leftover = ''
-count = 0
+doc_count = 0
+skip_doc_count = 0
+output_doc_count = 0
 
 while True:
-  count += 1
   if leftover == '':
     l = f.readline()
+    doc_count += 1
     if l == '':
       break
     l = l.strip().split('\t', 2)
     if len(l) != 3:
-      print 'failed to get 3 elems: %d, count %s, line %s; skipping' % (len(l), count, l)
+      #print('failed to get 3 elems: %d, doc count %s, line %s; skipping' % (len(l), doc_count, l))
+      skip_doc_count += 1
       continue
     else:
       title = l[0]
@@ -24,9 +31,11 @@ while True:
 
   if len(leftover) <= numCharLimit:
     fOut.write('%s\t%s\t%s\n' % (title, date, leftover))
+    output_doc_count += 1
     leftover = ''
   else:
     spot = numCharLimit
+    # TODO: this is super hackity!!  change to proper unicode word break splitting
     while spot >= 0 and leftover[spot] != ' ':
       spot -= 1
 
@@ -36,8 +45,10 @@ while True:
     chunk = leftover[:spot]
     fOut.write('%s\t%s\t%s\n' % (title, date, chunk))
     leftover = leftover[spot:]
+    output_doc_count += 1
 
-print '%d lines' % count
+print('%d input documents (%d skipped due to empty body text))' % (doc_count, skip_doc_count))
+print('%d output documents' % output_doc_count)
 
 f.close()
 fOut.close()

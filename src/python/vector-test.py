7e795b2cea1524365e81b7d76b3f62f367a8ff51
Michael Sokolov <sokolovm@amazon.com>
2023-05-22 14:37:38 +0000

    vector-test.py uses wikivector1m, no facet tasks

--- src/python/vector-test.py
+++ src/python/vector-test.py
@@ -22,19 +22,15 @@ import constants
 # simple example that runs benchmark with WIKI_MEDIUM source and task files 
 # Baseline here is ../lucene_baseline versus ../lucene_candidate
 if __name__ == '__main__':
-  #sourceData = competition.sourceData('wikivector1m')
+  sourceData = competition.sourceData('wikivector1m')
   #sourceData = competition.sourceData('wikivector10k')
-  sourceData = competition.sourceData('wikimedium10k')
-  comp =  competition.Competition()
+  #sourceData = competition.sourceData('wikimedium10k')
+  comp =  competition.Competition(verifyScores=False)
 
   index = comp.newIndex('baseline', sourceData,
                         vectorFile=constants.GLOVE_VECTOR_DOCS_FILE,
                         vectorDimension=100,
-                        facets = (('taxonomy:Date', 'Date'),
-                                  ('taxonomy:Month', 'Month'),
-                                  ('taxonomy:DayOfYear', 'DayOfYear'),
-                                  ('sortedset:Month', 'Month'),
-                                  ('sortedset:DayOfYear', 'DayOfYear')))
+                        vectorEncoding='FLOAT32')
 
   #Warning -- Do not break the order of arguments
   #TODO -- Fix the following by using argparser
@@ -49,14 +45,15 @@ if __name__ == '__main__':
                   index = index, concurrentSearches = concurrentSearches)
 
   # use a different index 
-  index = comp.newIndex('candidate', sourceData,
-                        vectorFile=constants.GLOVE_VECTOR_DOCS_FILE,
-                        vectorDimension=100,
-                        facets = (('taxonomy:Date', 'Date'),
-                                  ('taxonomy:Month', 'Month'),
-                                  ('taxonomy:DayOfYear', 'DayOfYear'),
-                                  ('sortedset:Month', 'Month'),
-                                  ('sortedset:DayOfYear', 'DayOfYear')))
+  # index = comp.newIndex('candidate', sourceData,
+  #                       vectorFile=constants.GLOVE_VECTOR_DOCS_FILE,
+  #                       vectorDimension=100,
+  #                       vectorEncoding='FLOAT32',
+  #                       facets = (('taxonomy:Date', 'Date'),
+  #                                 ('taxonomy:Month', 'Month'),
+  #                                 ('taxonomy:DayOfYear', 'DayOfYear'),
+  #                                 ('sortedset:Month', 'Month'),
+  #                                 ('sortedset:DayOfYear', 'DayOfYear')))
   # create a competitor named my_modified_version with sources in the ../patch folder
   # note that we haven't specified an index here, luceneutil will automatically use the index from the base competitor for searching 
   # while the codec that is used for running this competitor is taken from this competitor.

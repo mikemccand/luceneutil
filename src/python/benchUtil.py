a459508e43b5d9d57f431b96acbf931478e6bd02
Benjamin Trent <ben.w.trent@gmail.com>
2023-11-13 09:56:57 -0500

    Updating to optionally take advantage of int8 quantization (#244)
    * vector-test.py uses wikivector1m, no facet tasks
    
    * Account for new similarity functions
    
    removing unnecessary change
    
    * updating tester
    
    * fixing build.xml
    
    ---------
    
    Co-authored-by: Michael Sokolov <sokolovm@amazon.com>
    Co-authored-by: Michael McCandless <mikemccand@apache.org>

--- src/python/benchUtil.py
+++ src/python/benchUtil.py
@@ -1628,6 +1628,7 @@ def getClassPath(checkout):
   cp.append('%s/lucene/highlighter/build/classes/java/main' % path)
   cp.append('%s/lucene/codecs/build/classes/java/main' % path)
   cp.append('%s/lucene/queries/build/classes/java/main' % path)
+  cp.append('%s/lucene/join/build/classes/java/main' % path)
 
   # self.addJars(cp, '%s/lucene/facet/lib' % path)
 

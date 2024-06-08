eb873db0b8612587c30ea96cc65d87d9de74bf7f
Adrien Grand <jpountz@gmail.com>
2024-06-08 17:17:38 +0200

    Run nightly benchmarks with a single thread executor. (#271)
    We're making concurrenty search more of a first-class citizen, and hopefully it
    will soon even be the default in Lucene. So let's start better exercising code
    paths for concurrent search in nightly benchmarks by configuring an executor,
    but bounding it to a single thread for now to contain noise.
    
    Closes #210

--- src/python/example.py
+++ src/python/example.py
@@ -25,8 +25,8 @@ if __name__ == '__main__':
                                    description='Run a local benchmark on provided source dataset.')
   parser.add_argument('-s', '-source', '--source',
                       help='Data source to run the benchmark on.')
-  parser.add_argument('-concurrentSearches', '--concurrentSearches', action='store_true',
-                      help='Run concurrent searches')
+  parser.add_argument('-searchConcurrency', '--searchConcurrency',
+                      help='Search concurrency, 0 for disabled, -1 for using all cores')
   parser.add_argument('-b', '--baseline', default='lucene_baseline',
                       help='Path to lucene repo to be used for baseline')
   parser.add_argument('-c', '--candidate', default='lucene_candidate',
@@ -52,7 +52,7 @@ if __name__ == '__main__':
 
   # create a competitor named baseline with sources in the ../trunk folder
   comp.competitor('baseline', args.baseline,
-                  index = index, concurrentSearches = args.concurrentSearches)
+                  index = index, searchConcurrency = args.searchConcurrency)
 
   # use the same index as baseline unless --reindex was passed.
   # create a competitor named my_modified_version (or provided candidate name) with sources in the ../patch folder
@@ -72,7 +72,7 @@ if __name__ == '__main__':
                                   ('taxonomy:RandomLabel', 'RandomLabel'),
                                   ('sortedset:RandomLabel', 'RandomLabel')))
   comp.competitor('my_modified_version', args.candidate,
-                  index = candidate_index, concurrentSearches = args.concurrentSearches)
+                  index = candidate_index, searchConcurrency = args.searchConcurrency)
 
   # start the benchmark - this can take long depending on your index and machines
   comp.benchmark("baseline_vs_patch")

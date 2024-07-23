7dd6f308f0cb62028fc28f4673d69f6175cc9149
Vigya Sharma <vigyas@amazon.com>
2024-07-23 13:52:15 -0700

    minor bug fixes

--- src/python/infer_token_vectors_cohere.py
+++ src/python/infer_token_vectors_cohere.py
@@ -1,8 +1,24 @@
+#!/usr/bin/env python
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 import os
 import datasets
 import numpy as np
-import sys
 
 import localconstants
 
@@ -12,16 +28,13 @@ the Cohere/wikipedia-22-12-en-embeddings https://huggingface.co/datasets/Cohere/
 
 Usage: 
 
-python src/python/infer_token_vectors_cohere.py <result_docs_vector_file> <num_docs> <result_queries_vector_file> <num_queries>
+python src/python/infer_token_vectors_cohere.py -d <num_docs> -q <num_queries>
+python src/python/infer_token_vectors_cohere.py -n <filename_prefix> -d <num_docs> -q <num_queries>
 
-python src/python/infer_token_vectors_cohere.py ../data/cohere-wikipedia-768.vec 1000000 \
-    ../data/cohere-wikipedia-queries-768.vec 10000
+For help:
+python src/python/infer_token_vectors_cohere.py -h
 """
 
-# filename = sys.argv[1]
-# num_docs = int(sys.argv[2])
-# filename_queries= sys.argv[3]
-# num_queries = int(sys.argv[4])
 DATASET_PATH = 'Cohere/wikipedia-22-12-en-embeddings'
 DIMENSIONS = 768
 
@@ -75,8 +88,8 @@ def fetch_cohere_vectors():
     #print(f'\nds_embs: {ds_embs}')
     embs = np.array(ds_embs, dtype=np.float32)
     print(f'embs: {embs.dtype} {embs.size} {embs.itemsize} {embs.shape}')
-    print(f'wiki_id: {ds_wiki_id.shape}')
-    print(f'para_id: {ds_para_id.shape}')
+    print(f'wiki_id: {len(ds_wiki_id)}')
+    print(f'para_id: {len(ds_para_id)}')
 
     print(f"saving docs[{doc_upto}:{next_doc_upto}] of shape: {embs.shape} to file")
     with open(doc_file, "ab") as out_f:
@@ -111,7 +124,8 @@ def fetch_cohere_vectors():
   with open(meta_file, 'rt') as meta:
     md = meta.readlines()
   print(f'metadata file row_count: {len(md)}, includes 1 header line')
-  print(md[:4])
+  for line in md[:11]:
+    print(line.rstrip('\n'))  # print() adds another newline
 
 if __name__ == '__main__':
   fetch_cohere_vectors()

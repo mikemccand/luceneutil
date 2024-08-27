bf4fb628949b1831a33d17c6d526ff960c3e5b2a
Mike McCandless <mikemccand@apache.org>
2024-08-27 11:06:53 -0400

    Fix broken geo corpus link

--- src/python/writeGeoGraphs.py
+++ src/python/writeGeoGraphs.py
@@ -321,7 +321,7 @@ with open('/l/lucenenightly/docs/geobench.html', 'w') as f:
 <h2>Lucene Geo benchmarks</h2>
 <div style="min-width: 800px">
 <p>Below are the results of the Lucene nightly geo benchmarks based on the <a href="https://gitbox.apache.org/repos/asf/lucene.git">main</a> branch as of that point in time.</p>
-<p>This test indexes a 60.8M point subset exported from the full (as of 3/7/2016) 3.2B point <a href="http://openstreetmaps.org">OpenStreetMaps corpus</a>, including every point inside London, UK, and 2.5% of the remaining points, using three different approaches, and then tests search and sorting performance of various shapes.  The London boroughs polygons <a href="http://data.london.gov.uk/2011-boundary-files">come from here</a> (33 polygons, average 5.6K vertices).</p>
+<p>This test indexes a 60.8M point subset exported from the full (as of 3/7/2016) 3.2B point <a href="http://openstreetmaps.org">OpenStreetMaps corpus</a>, including every point inside London, UK, and 2.5% of the remaining points, using three different approaches, and then tests search and sorting performance of various shapes.  The London boroughs polygons <a href="https://data.london.gov.uk/dataset/statistical-gis-boundary-files-london">come from here</a> (33 polygons, average 5.6K vertices).</p>
 <p>On each chart, you can click + drag (vertically or horizontally) to zoom in and then shift + drag to move around, and double-click to reset.  Hover over an annotation to see known changes.  Click on a point to see the exact source git revision that was run.</p>
 </div>
     ''')

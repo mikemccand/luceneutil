fe48ae66590ba79985c76df4482cbe78a692a9f1
Adrien Grand <jpountz@gmail.com>
2025-08-13 16:30:17 +0200

    Add license and tidy a bit.

--- src/main/perf/TypePolluter.java
+++ src/main/perf/TypePolluter.java
@@ -1,5 +1,22 @@
 package perf;
 
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,13 +48,15 @@ import org.apache.lucene.store.ByteBuffersDirectory;
 import org.apache.lucene.store.Directory;
 
 /**
- * This helper tries to pollute a bit the types that are typically seen by queries at call sites to help better simulate production systems that may
+ * This helper tries to pollute a bit the types that are typically seen by queries at call sites to
+ * help better simulate production systems that may
  * <ul>
  *   <li>have a mix of Directory impls, e.g. because of NRTCachingDirectory</li>
  *   <li>have a mix of segments with deletions and no deletions,</li>
  *   <li>use multiple similarities.</li>
  * </ul>
- * <p>This matters because polymorphic call sites are much more expensive than bimorphic call sites, and bimorphic call sites may be noticeably more expensive than monomorphic call sites.
+ * <p>This matters because polymorphic call sites are much more expensive than bimorphic call sites,
+ * and bimorphic call sites may be noticeably more expensive than monomorphic call sites.
  */
 public class TypePolluter {
 
@@ -46,7 +65,7 @@ public class TypePolluter {
     try (Directory dir = new ByteBuffersDirectory()) {
 
       // TODO: configure a non-default codec?
-      IndexWriterConfig config = new IndexWriterConfig(null);;
+      IndexWriterConfig config = new IndexWriterConfig(null);
 
       try (IndexWriter w = new IndexWriter(dir, config)) {
         // Add enough documents for the inverted index to have full blocks (128 postings)

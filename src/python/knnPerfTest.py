aa532306dcc8bcc37a45fe27b78cdecb00443ee4
Alec Bernardi <alecber@amazon.com>
2026-01-06 15:57:46 +0000

    Add overSample to ignored labels in graph

--- src/python/knnPerfTest.py
+++ src/python/knnPerfTest.py
@@ -540,6 +540,7 @@ def remove_common_args(argmaps):
   # TODO: also remove other "minor" dimensions such as beam_width and maxconn?
   # or place under user control somehow
   common_args["-fanout"] = 1
+  common_args["-overSample"] = 1
   # everything remaining is in common to all rows, now remove them
   unique_args = []
   for args in argmaps:

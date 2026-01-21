a3a98d9794fba66e89de2445cf4019325b0eb096
Alex Baranov <olexandb@amazon.com>
2026-01-21 17:31:22 +0000

    refactor(generateNADTaxonomies): Make taxonomy generator reusable
    - Wrap logic in generate_nad_taxonomy() function for import use
    - Update to NAD r21 dataset format
    - Add configurable base_dir and input_file parameters
    - Add CLI argument parsing for standalone execution
    - Use os.path for portable path handling

--- src/python/generateNADTaxonomies.py
+++ src/python/generateNADTaxonomies.py
@@ -26,27 +26,53 @@ COUNTY_INDEX = 2
 STREETNAME_INDEX = 15
 ADDNAME_INDEX = 20
 
-with ZipFile("../../../data/NAD_r8_TXT.zip", "r") as zip:
-  with zip.open("TXT/NAD_r8.txt", "r") as nad:
-    with gzip.open("../../../data/NAD_taxonomy.txt.gz", "wb") as out:
-      i = 0
-      skipped_lines = 0
-      for line in nad:
-        if i % 100000 == 0:
-          print("Processed ", i, " lines")
-        str_line = line.decode("utf-8")
-        line_arr = str_line.split(",")
-        if len(line_arr) < 21:
-          skipped_lines += 1
-          continue
-        hierarchy = (line_arr[STATE_INDEX], line_arr[COUNTY_INDEX], line_arr[STREETNAME_INDEX], line_arr[ADDNAME_INDEX])
-        if hierarchy[2] == "":
-          hierarchy = (hierarchy[0], hierarchy[1], "NONE", hierarchy[3])
-        if hierarchy[3] == "":
-          hierarchy = (hierarchy[0], hierarchy[1], hierarchy[2], "NONE")
-        str_hierarchy = hierarchy[0] + "," + hierarchy[1] + "," + hierarchy[2] + "," + hierarchy[3] + "\n"
-        out.write(str_hierarchy.encode("utf8"))
-        i += 1
-      print("Read", i, "lines")
-      print("Skipped", skipped_lines, "lines")
-      print("NAD_taxonomy.txt.gz contains", os.stat("../../../data/NAD_taxonomy.txt.gz").st_size, "bytes of compressed data")
+
+def generate_nad_taxonomy(base_dir=None, input_file="NAD_r21_TXT.zip"):
+  """Generate NAD taxonomy file from the NAD zip archive.
+
+  Args:
+    base_dir: Base directory containing the data folder. If None, defaults to
+              BASE_DIR env var or relative path from this script.
+    input_file: Name of the NAD zip file to process. Defaults to NAD_r21_TXT.zip.
+  """
+  if base_dir is None:
+    base_dir = os.environ.get("BASE_DIR", os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+
+  input_zip = os.path.join(base_dir, "data", input_file)
+  output_file = os.path.join(base_dir, "data", "NAD_taxonomy.txt.gz")
+
+  with ZipFile(input_zip, "r") as zip:
+    with zip.open("TXT/NAD_r21.txt", "r") as nad:
+      with gzip.open(output_file, "wb") as out:
+        i = 0
+        skipped_lines = 0
+        for line in nad:
+          if i % 100000 == 0:
+            print("Processed ", i, " lines")
+          str_line = line.decode("utf-8")
+          line_arr = str_line.split(",")
+          if len(line_arr) < 21:
+            skipped_lines += 1
+            continue
+          hierarchy = (line_arr[STATE_INDEX], line_arr[COUNTY_INDEX], line_arr[STREETNAME_INDEX], line_arr[ADDNAME_INDEX])
+          if hierarchy[2] == "":
+            hierarchy = (hierarchy[0], hierarchy[1], "NONE", hierarchy[3])
+          if hierarchy[3] == "":
+            hierarchy = (hierarchy[0], hierarchy[1], hierarchy[2], "NONE")
+          str_hierarchy = hierarchy[0] + "," + hierarchy[1] + "," + hierarchy[2] + "," + hierarchy[3] + "\n"
+          out.write(str_hierarchy.encode("utf8"))
+          i += 1
+        print("Read", i, "lines")
+        print("Skipped", skipped_lines, "lines")
+        print("NAD_taxonomy.txt.gz contains", os.stat(output_file).st_size, "bytes of compressed data")
+
+
+if __name__ == "__main__":
+  import argparse
+  parser = argparse.ArgumentParser(description="Generate NAD taxonomy file from NAD zip archive")
+  parser.add_argument("input_file", nargs="?", default="NAD_r21_TXT.zip",
+                      help="NAD zip file to process (default: NAD_r21_TXT.zip)")
+  parser.add_argument("--base-dir", default=None,
+                      help="Base directory containing the data folder")
+  args = parser.parse_args()
+  generate_nad_taxonomy(args.base_dir, args.input_file)
